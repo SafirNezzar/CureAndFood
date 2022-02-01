44fc373280d9ded9b930fc678129111c79ca202f
SafirNezzar <safirnezzar@gmail.com>
2022-02-01 18:44:55 +0100

    1er jour création page accueil

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{   
+    // Accueil
+    #[Route('', name: 'home_page')]
+    public function home(): Response
+    {
+        return $this->render('base/homePage.html.twig', [
+            
+        ]);
+    }
+
+    // A propos
+    
+}
